ac6ecfd538243ebb8283c2e9c0a021bff3b649f1
Jeroen P. Broks <facebook@tbbs.nl>
2020-08-24 10:09:24 +0200

    vUpdate

--- Source/SDL_Manager.cpp
+++ Source/SDL_Manager.cpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 20.08.21
+// Version: 20.08.24
 // EndLic
 
 
