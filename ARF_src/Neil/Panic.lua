625bc6c1f41169e313dcf64943c7e6e07cb9ec86
Jeroen P. Broks <facebook@tbbs.nl>
2020-08-26 21:31:06 +0200

    PANIEK! PANIEK! PANIEK! PEEUW! PEEUW! PEEUW! PEEUW!

--- ARF_src/Neil/Panic.lua
+++ ARF_src/Neil/Panic.lua
@@ -1,6 +1,6 @@
 -- <License Block>
--- barf.lua
--- Build Apollo Resource File
+-- ARF_src/Neil/Panic.lua
+-- Apollo - Panic
 -- version: 20.08.26
 -- Copyright (C) 2020 Jeroen P. Broks
 -- This software is provided 'as-is', without any express or implied
@@ -17,14 +17,7 @@
 -- misrepresented as being the original software.
 -- 3. This notice may not be removed or altered from any source distribution.
 -- </License Block>
-local PackAs = "Store"
-
-JLS.Output("FATSTORAGE:Store") -- Might be "lzma" or "zlib" later depending on how things go here.
-JLS.SetJCR6OutputFile("x64/Release/Apollo Game Engine.arf")
-
--- Add("ARF_src","",{Author="Jeroen P. Broks",Notes="All copyrighted by Jeroen P. Broks and zlib licensed unless explicity stated otherwise in documentation",Storage=PackAs})
-Add("ARF_src/ID","ID",{Author="Jeroen P. Broks",Notes="All copyrighted by Jeroen P. Broks and zlib licensed",Storage=PackAs})
-Add("ARF_src/Pics","Pics",{Author="Read documentation",Notes="Read Documentation",Storage=PackAs})
-Add("ARF_src/Fonts","Fonts",{Author="Volker Busse",Notes="Donated to DaFont as 100% free",Storage=PackAs})
-Add("../../../lua/Neil/Neil.lua","Neil/Neil.lua",{Author="Jeroen P. Broks", Notes="zlib Licensed", Storage=PackAs})
-Add("ARF_src/Neil","Neil",{Author="Jeroen P. Broks",Notes="zlib Licensed",Storage=PackAs})
+local Apollo_Crash=Apollo_Crash
+function Apollo_Panic(errormessage)
+	Apollo_Crash(errormessage,Neil.Globals.ApolloState.Name,Neil.Globals.ApolloState.TraceBack)
+end
