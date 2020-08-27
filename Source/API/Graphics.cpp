2449f12ed0372adc86b9a044e8f01627adeccfb9
Jeroen P. Broks <facebook@tbbs.nl>
2020-08-27 21:06:16 +0200

    Work Work Work!

--- Source/API/Graphics.cpp
+++ Source/API/Graphics.cpp
@@ -25,8 +25,63 @@
 // EndLic
 // We get into the deep of this later!
 
-namespace Tricky_Apollo {
-	void ApolloAPIInit_Graphics() {
+#include <AP_Lua_CPP.hpp>
+#include <TQSG.hpp>
+#include <States.hpp>
 
+namespace Tricky_Apollo {
+	using namespace TrickyUnits;
+
+	// General graphics features
+	static int AGGA_Cls(lua_State* L) {
+		TQSG_Cls();
+		return 0;
+	}
+
+	static int AGGA_ClsColor(lua_State* L) {
+		int r = 0, g = 0, b = 0;
+		r = luaL_checkinteger(L, 1);
+		g = luaL_checkinteger(L, 2);
+		b = luaL_checkinteger(L, 3);
+		TQSG_ClsColor(r % 255, g % 255, b % 255);
+		return 0;
+	}
+
+	static int AGGA_Color(lua_State* L) {
+		int r = 0, g = 0, b = 0;
+		r = luaL_checkinteger(L, 1);
+		g = luaL_checkinteger(L, 2);
+		b = luaL_checkinteger(L, 3);
+		TQSG_Color(r % 255, g % 255, b % 255);
+		return 0;
+	}
+
+	static int AGGA_GetColor(lua_State* L) {
+		Uint8 r = 0, g = 0, b = 0;
+		TQSG_GetColor(&r, &b, &b);
+		lua_Integer rr = r, rg = g, rb = b;
+		lua_pushinteger(L,rr);
+		lua_pushinteger(L,rg);
+		lua_pushinteger(L,rb);
+		return 3;
+	}
+
+	static int AGGA_Alpha(lua_State* L) {
+		if (lua_gettop(L)) TQSG_SetAlpha(luaL_checkinteger(L, 1));
+		lua_pushinteger(L, TQSG_GetAlpha());
+		return 1;
+	}
+
+
+
+	// Images
+
+	// ImageFont
+
+
+	void ApolloAPIInit_Graphics() {
+		Apollo_State::RequireFunction("AGGA_Cls", AGGA_Cls);
+		Apollo_State::RequireFunction("AGGA_ClsColor", AGGA_ClsColor);
+		Apollo_State::RequireNeil("API/Graphics.neil");
 	}
 }
