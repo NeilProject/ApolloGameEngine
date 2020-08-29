667a1fbe5d5b619035fcbcb1a18532e325bd5edc
Jeroen P. Broks <facebook@tbbs.nl>
2020-08-29 13:48:40 +0200

    vUpdate

--- APIHeader.go
+++ APIHeader.go
@@ -1,7 +1,7 @@
 // License Information:
 // APIHeader.go
 // APIHeader generator
-// version: 20.08.28
+// version: 20.08.29
 // Copyright (C) 2020 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
