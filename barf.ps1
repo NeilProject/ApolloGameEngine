d0d8a8d72ac5a00f54e9b258e4460ac4193c964d
Jeroen Broks <facebook@tbbs.nl>
2020-12-28 02:12:46 +0100

    Temp solution (I hope)

--- barf.ps1
+++ barf.ps1
@@ -3,5 +3,7 @@ echo "Building Apollo Resource File!"
 #cd ARF_src
 #njcr add -i . -doj -cm Store -fc Store -author "Jeroen P. Broks" -notes "zlib license unless stated otherwise in documentation" "x64/Release/Apollo Game Engine.arf"
 #jcr6 add -i ARF_src -doj -cm Store -fc Store -author "Jeroen P. Broks" -notes "zlib license unless stated otherwise in documentation" "x64/Release/Apollo Game Engine.arf"
-njcr script "E:\Projects\Applications\VisualStudio\VC\Apollo Game Engine\barf.lua"
+#njcr script "E:\Projects\Applications\VisualStudio\VC\Apollo Game Engine\barf.lua"
+cd "Scyndi:/Projects/Applications/VisualStudio/VC/Apollo Game Engine/"
+jcr6_script barf.lua
 #popd
