7785594bfbaa2906d252f75d0b4b9374454e73f2
Jeroen Broks <facebook@tbbs.nl>
2021-09-19 21:38:45 +0200

    Giant vUpdate

--- Source/API/Audio.cpp
+++ Source/API/Audio.cpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 21.02.14
+// Version: 21.09.19
 // EndLic
 // Lua
 #include <AP_Lua_CPP.hpp>
