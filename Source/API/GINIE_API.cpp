5fbe06ec6c279aea331e1594da0b8d2a785d8258
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-07-18 15:47:26 +0200

    vUpdate

--- Source/API/GINIE_API.cpp
+++ Source/API/GINIE_API.cpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 22.05.10
+// Version: 22.07.18
 // EndLic
 #include <AP_Lua_CPP.hpp>
 #include <GINIE.hpp>
